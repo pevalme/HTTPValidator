9f7161f09a5a29733ee255e38c2332d5ac94418a
Pedro <pedro.valero.mejia@gmail.com>
2017-01-13 15:36:03 +0100

    Adding license text to each file

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,21 @@
 #
 # Date: 1/4/16
 
+# HTTP Validator. Copyright (C) 2016  Pedro Valero
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 TMP="tmp.txt"
 AUX="tmp2.txt"
 # HHTPolice vs Our Implementation
